bce13e664be69f3ddd765212601d3cab972300e4
Jason Wang <jasonherngwang@gmail.com>
2023-03-10 12:39:29 -0800

    feat: exclamation

--- src/app.ts
+++ src/app.ts
@@ -20,7 +20,7 @@ app.get('/seamless', (_req, res) => {
 app.post('/notifications', async (req, res) => {
   try {
     const message = req.body.message;
-    await axios.post(NOTIFICATION_ENDPOINT, { message: message + '.' });
+    await axios.post(NOTIFICATION_ENDPOINT, { message: message + '!!!!' });
 
     res.status(200).json({ message: 'Notification was sent to the customer' });
   } catch (error) {
